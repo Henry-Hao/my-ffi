2592185ef158d3a19dbc7d77a44c636a36ba81ea
Yixian Hao <haoyixian1994@hotmail.com>
2021-09-07 01:37:59 +0800

    1. Modifed callback.c and main.rs:     - Pass a Rust object in the callback function.     - Modify the object in the callback function in C library.

--- src/callback.c
+++ src/callback.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef void(*rust_callback)(int32_t);
+typedef void(*rust_callback)(void*, int32_t);
+
+void* cb_target;
 rust_callback cb;
 
-int32_t register_callback(rust_callback callback) {
+int32_t register_callback(void* target, rust_callback callback) {
     cb = callback;
+    cb_target = target;
     return 1;
 }
 
 void trigger_callback() {
-    cb(3);
+    cb(cb_target,3);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 #[link(name="callback", kind="static")]
 extern "C"{
-    fn register_callback(cb: extern fn(i32)) -> i32;
+    fn register_callback(target: *mut Target, cb: extern fn(*mut Target, i32)) -> i32;
     fn trigger_callback();
 }
 
-extern fn callback(i: i32) {
-    println!("Called from rust {}", i);
+#[repr(C)]
+#[derive(Debug)]
+pub struct Target {
+    a: i32
+}
+
+extern fn callback(target: *mut Target, i: i32) {
+    unsafe {
+        println!("Before:{:?}", (*target).a);
+        (*target).a = i;
+        println!("After:{:?}", (*target).a);
+    }
 }
 
 
 fn main() {
     unsafe {
-        register_callback(callback);
+        let mut t = Box::new(Target{
+            a: 0
+        });
+        register_callback(&mut *t, callback);
         trigger_callback();
     }
 }
